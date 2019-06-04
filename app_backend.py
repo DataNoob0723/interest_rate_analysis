10090b4431818df4fac8188908f0a5bba3084de2
Ryan <ryanburdick14@gmail.com>
2019-06-03 21:12:27 -0500

    changes

--- app_backend.py
+++ app_backend.py
@@ -25,6 +25,7 @@ import json
 import os
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split, KFold, cross_val_score
+from sklearn.metrics import confusion_matrix
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
@@ -34,11 +35,25 @@ from keras.layers import Input, Dense, Dropout, LSTM
 from keras.models import Model, Sequential
 from keras import optimizers
 from keras.utils import to_categorical
+from sqlalchemy import create_engine
+import pymysql
 
 
 class Model_Generation:
     def __init__(self):
-        self.df = pd.read_csv("data/correct_fred_data.csv")
+#        self.df = pd.read_csv("data/correct_fred_data.csv")
+        pymysql.install_as_MySQLdb()
+        
+        PASSWD = os.getenv("LOCAL_MYSQL_PASS")
+        USER = "root"
+        PORT = "3306"
+        DOMAIN = "localhost"
+        DB = "fred_db"
+        engine = create_engine(f"mysql://{USER}:{PASSWD}@{DOMAIN}:{PORT}/{DB}")
+        self.conn = engine.connect()
+        
+        self.df = pd.read_sql_query('select * from fred_data', con=engine)
+        
     
     def preprocess_df(self):
         """
@@ -88,7 +103,7 @@ class Model_Generation:
         reversed_df.reset_index(drop=True, inplace=True)
         
         return reversed_df
-    
+        
     def get_dates(self):
         """
         Return a list of all of the available dates from reversed_df
@@ -216,11 +231,11 @@ class Model_Generation:
         
         # Spot-check algorithms
         models = []
-        models.append(('LR', LogisticRegression())) 
+        models.append(('LR', LogisticRegression(class_weight="balanced"))) 
         models.append(('KNN', KNeighborsClassifier())) 
-        models.append(('CART', DecisionTreeClassifier())) 
+        models.append(('CART', DecisionTreeClassifier(class_weight="balanced"))) 
         models.append(('NB', GaussianNB())) 
-        models.append(('SVM', SVC(probability=True)))
+        models.append(('SVM', SVC(class_weight="balanced", probability=True)))
     
         results = []
         names = []
@@ -240,6 +255,9 @@ class Model_Generation:
             # Evaluate the model on the test sets
             result = model.score(X_test, Y_test)
             print(f"Test set acc: {result}")
+            
+            # Print confusion matrix
+            print(confusion_matrix(Y_test, model.predict(X_test)))
             
             with open(f"data/json_files/dates/classifiers/{name}/dates.json", "w") as json_file:
                 json.dump(dates_list, json_file)
@@ -382,6 +400,16 @@ class Model_Generation:
         col_list = list(df_X.columns)
         
         return col_list
+    
+    def value_counts(self, x):
+        """
+        x is an 1-D np array
+        """
+        y = np.bincount(x.astype(int))
+        ii = np.nonzero(y)[0]
+        print(list(zip(ii, y[ii])))
+
+        
         
 
 if __name__ == "__main__":
@@ -391,3 +419,7 @@ if __name__ == "__main__":
     MG.non_NN_algorithms_spot_check(2)
     MG.naive_NN_classifier(2)
     MG.LSTM_classifier(7)
+    
+#    X_train, X_test, Y_train, Y_test = MG.train_test_gen(2)
+#    MG.value_counts(Y_train)
+    
